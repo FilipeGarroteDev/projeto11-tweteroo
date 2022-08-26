f0aed3e1b173ce83c0456f4ed8b9e5f7c37aa1a4
Filipe Garrote <filipe.garrote@gmail.com>
2022-08-26 11:26:24 -0300

    feat(post login) - Add armazenamento de dados do usuário, ao fazer um post

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ import cors from "cors";
 
 const server = express();
 server.use(cors());
+server.use(express.json());
+
+const users = []
+
+
+server.post("/sign-up", (req, res) => {
+  const newUser = req.body
+  users.push(newUser)
+  res.send(newUser)
+  console.log(newUser)
+})
+
+
 
 server.listen(5000, function(){
   console.log("Listening to port 5000")
